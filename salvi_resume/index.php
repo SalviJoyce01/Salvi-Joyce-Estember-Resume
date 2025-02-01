cc3f3b8596db62cabcc0af3c1a8a2002e779d76f
Salvi_Joyce <estemberoyens@gmail.com>
2025-02-01 22:22:57 +0800

    Add files via upload

---
+++ salvi_resume/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estember_Resume</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Salvi Joyce V. Estember</h1>
+            <p>Email: estembersalvi@gmail.com | Phone: 09912345678</p>
+            <p>Location: Sn. Miguel, Jordan, Guimaras</p>
+            <image src="salvi.JPG" alt="Salvi Joyce V. Estember">
+        </header>
+
+        <section class="summary">
+            <h2>Professional Summary</h2>
+            <p>As a recent graduate of BS Information Technology with a degree of Web and Mobile Development, 
+                I'm excited to launch my career in UX & UI design. 
+                Skilled in Adobe Creative Suite, Figma, and Microsoft Office, 
+                I'm passionate about creating intuitive and 
+                user-friendly interfaces for web and mobile applications. 
+                Seeking a role where I can grow my skills and contribute to a dynamic team.
+            </p>
+        </section>
+
+        <!-- Jobs -->
+        <section class="experience">
+            <h2>Work Experience</h2>
+            <div class="job_1">
+                <h3>Junior Designer - APEX Company</h3>
+                <p><strong>Cebu, Philippines</strong> | May 2027 - June 2029</p>
+                <p>Responsible for ideating and execution of AIGA event graphics and professional communication assets.
+                </p>
+            </div>
+            <br>
+            <div class="job_2">
+                <h3>Graphic Designer - University of the Philippines</h3>
+                <p><strong>Cebu, Philippines</strong> | May 2029 - June 2030</p>
+                <p>Design print and digital marketing imagery promoting holistic wellness for University's health initiatives.
+                </p>
+            </div>
+            <br>
+            <div class="job_3">
+                <h3>Visual Design Intern - Digital Designing Company</h3>
+                <p><strong>Ontario, Canada</strong> | May 2030 - Present</p>
+                <p>Designed product packaging, created online campaigns, and worked on UI development for web/mobile.
+                </p>
+            </div>
+        </section>
+
+        <!-- Education -->
+        <section class="education_1">
+            <h2>Education</h2>
+            <div class="school_1">
+                <h3>Guimaras State University - Mosqueda Campus</h3>
+                <p><Strong>Alaguisoc, Jordan, Guimaras</Strong> <br>Class of 2026 <br> BS Information Technology <br> Major in Mobile and Web Development</p>
+            </div>
+            <br>
+            <div class="school_2">
+                <h3>International Technology University</h3>
+                <p><Strong>Cebu City, Phillipines</Strong> <br> Class of 2028 <br> Masters in Mobile and Web Development
+                </p>
+            </div>
+        </section>
+
+        <!-- Recognitions and Awards -->
+        <section class="recognitions">
+            <h2>Recognition and Awards</h2>
+            <ul>
+                <li>Dean's Lister 2022-2026</li>
+                <li>Graphic International Design Awards</li>
+                <li>International Design Excellence Awards(IDEA)</li>
+                <li>Cybersecurity Training Awards</li>
+                <li>Google IT Support Professional Certificate</li>
+            </ul>
+        </section>
+
+        <!--skills -->
+        <section class="skills">
+            <h2>Skills</h2>
+            <ul>
+                <li>Front-end Development</li>
+                <li>User Interface Design</li>
+                <li>User Experience Design</li>
+                <li>Web Designing</li>
+                <li>Mobile App Designing</li>
+            </ul>
+        </section>
+
+        <!--Reference-->
+        <section class="Reference">
+            <h2>Reference</h2>
+            <h4>MRS. LINDSEY M. POTTER</h4>
+            <p>University of the Phillipines<br>College IT Head Teacher<br>09987654321</p>
+    </div>
+</body>
+</html>
